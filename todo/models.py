f9151e458c00031d066a5795418cf7ee97fac834
Robin A <robinand@live.com>
2023-04-30 14:36:38 +0000

    add edit and add item templates

--- todo/models.py
+++ todo/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 
 # First model(Item)
+
+
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
