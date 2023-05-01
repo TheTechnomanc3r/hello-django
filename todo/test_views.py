7662eae7761658ca8c97b580d2303c461ac6fc3f
Robin A <robinand@live.com>
2023-05-01 21:37:54 +0000

    add form tests

---
+++ todo/test_views.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+
+class TestViews(TestCase):
+
+    def test_get_todo_list(self):
+        response = self.client.get("/")
+
+    def test_get_add_item_page(self):
+
+    def test_get_edit_item_page(self):
+
+    def test_can_add_item(self):
+
+    def test_can_toggle_item(self):    
+
+                  
